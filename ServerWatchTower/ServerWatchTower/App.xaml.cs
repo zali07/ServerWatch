c8ff49cf53d1dba768b9c581e0c13489dae1f16a
Füzi Zalán <fuzi.zalan@gmail.com>
2025-04-09 09:40:48 +0300

    WPF project with SilverLib.

---
+++ ServerWatchTower/ServerWatchTower/App.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Cosys.SilverLib.Core;
+using Cosys.SilverLib.Model;
+using System;
+using System.Collections.Generic;
+
+namespace ServerWatchTower
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="App"/> class.
+        /// </summary>
+        public App()
+        {
+            this.ApplicationRegistryKey = @"Software\Cosys\ServerWatchTower";
+            this.ApplicationTitle = "ServerWatchTower";
+        }
+
+        protected override void SetUpApplicationTheme()
+        {
+            Telerik.Windows.Controls.RadRibbonWindow.IsWindowsThemeEnabled = false;
+            Telerik.Windows.Controls.StyleManager.ApplicationTheme = new Telerik.Windows.Controls.Office_BlackTheme();
+        }
+
+        protected override List<Type> GetCoreCompositionTypes(IConnectionProvider connectionProvider)
+        {
+            return new List<Type>()
+            {
+                typeof(Cosys.SilverLib.View.TelerikReports.TelerikReportHandler)
+            };
+        }
+
+        //protected override IEnumerable<IModule> CreateModuleInstances()
+        //{
+        //    return new SingleItemEnumerator<IModule>(new PersonalFPModule());
+        //}
+
+        protected override SessionInfo AuthenticateUser()
+        {
+            return new WindowsSessionInfo();
+        }
+
+        //protected override void InitializeMainWindow(MainWindow window)
+        //{
+        //    var iconUri = new Uri("pack://application:,,,/PersonalFP.Silver;component/money.ico");
+        //    window.Icon = BitmapFrame.Create(iconUri);
+        //}
+    }
+}
